158df8d446f7849d65c914524b4d591e16da6191
Fritz Ramirez <keemfzky@gmail.com>
2025-10-22 08:49:32 +0800

    Renamed test motion

--- demo/curators/Shelf.jsx
+++ demo/curators/Shelf.jsx
@@ -26,7 +26,7 @@ export function Shelf() {
     ];
 
     return (
-        <div layout="shelf" motion="test">
+        <div layout="shelf" motion="fade-in-up">
             {cards.map(props => <Card {...props} />)}
         </div>
     );
